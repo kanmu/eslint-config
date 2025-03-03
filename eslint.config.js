7fe4bcaff083f7c95f70b88a78496c1892260edc
moqada <moqada@gmail.com>
2025-03-03 10:54:01 +0900

    chore: Update eslint.config.js
    - Flat config ベースに置き換えた
    - Shareable config は Node.js 前提なので、node.js の設定を取り込むことにした
    - 設定にはマジックナンバーを多用するので no-magic-numbers は無効化した

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,27 @@
-env:
-  node: true
+import mainConfig from './index.js';
+import nodeConfig from './node.js';
+
+export default [
+  ...mainConfig,
+  ...nodeConfig,
+  {
+    rules: {
+      // マジックナンバーはルール設定上頻出するので許容する
+      'no-magic-numbers': 0,
+      // ルールの整列方針が定まっておらず、旧記法を引き継ぐために一時的に無効化する
+      'sort-keys': 0
+    },
+    settings: {
+      n: {
+        allowModules: [
+          'eslint-plugin-flowtype',
+          'eslint-plugin-import',
+          'eslint-plugin-jsdoc',
+          'eslint-plugin-n',
+          'eslint-plugin-react',
+          'eslint-plugin-react-native'
+        ]
+      }
+    }
+  }
+];
